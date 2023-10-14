655185fc237f1f62f812b09ca4bd5813b1a87552
Magdalena Stryszowska <madzi13@op.pl>
2023-10-14 21:37:07 +0200

    stoper_2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-const App = () => {
-  return (
-    <h1>Hello world</h1>
-  );
-};
+import Container from './components/Container/Container.js';
+import Button from './components/Button/Button.js';
+import FormattedTime from './components/FormattedTime/FormattedTime.js';
+import {useState, useEffect} from 'react'; 
 
+const App = () => {
+
+  const [time, setTime] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+
+
+
+  const startTimer = () => {
+    const id = setInterval(() => {
+      setTime((prevValue) => prevValue + 1);
+    }, 1);
+    setIntervalId(id);
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+  };
+
+  const resetTimer = () => {
+    setTime(0);
+  };
+
+  useEffect(() => {
+    return () => {
+       if(intervalId) clearInterval(intervalId);
+    };
+  
+  }, []);
+  return (
+  <Container>
+  <FormattedTime time={time} />
+  <Button onClick={startTimer}>Start</Button>
+  <Button onClick={stopTimer}>Stop</Button>
+  <Button onClick={resetTimer}>Reset</Button>
+</Container>
+);
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+createRoot(document.querySelector('#root')).render(<App />);
